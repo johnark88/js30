30083fa3c482283335b477f1000053cb4b313bbb
John Arkema <john.arkema@gmail.com>
2017-02-15 18:45:49 -0600

    add sep js file

---
+++ 01 - JavaScript Drum Kit/client.js
@@ -0,0 +1,9 @@
+console.log('sourced');
+    window.addEventListener('keydown', function(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if (!audio) return; //stop the function
+        audio.currentTime = 0; //rewind
+        audio.play();
+        key.classList.add('playing')
+    });
