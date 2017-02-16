eaf46dc26130d3d912756045084983141d6b64e2
John Arkema <john.arkema@gmail.com>
2017-02-15 19:03:07 -0600

    works / no sep js file

--- 01 - JavaScript Drum Kit/client.js
+++
@@ -1,9 +0,0 @@
-console.log('sourced');
-    window.addEventListener('keydown', function(e) {
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        if (!audio) return; //stop the function
-        audio.currentTime = 0; //rewind
-        audio.play();
-        key.classList.add('playing')
-    });
